5025f9c1f77b1dc4b9d7e3876667606de179d7a1
AUGUSTO AMADEO TRAVERSO <49315675@ort>
2025-09-18 08:03:08 -0300

    INICIO

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PrimerProyecyo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
